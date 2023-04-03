9364f6d58ff5aa59d0da8c827b9df3e1664cb8a9
sasimopuri <sasikmopuriu@gmail.com>
2023-04-03 15:15:20 +0530

    rendered restuarents cards

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,14 @@
-// import Restro_Cards from "./Restro_Card"
+import Restro_Cards from "./Restro_Card"
 import { restro_img } from "../utils/constants"
+import { restro_data } from "../utils/restro_data"
 export default function Body(){
+    const card=restro_data?.map((data)=><Restro_Cards key={data.id} value={data.data} />)
+    // console.log("s",card);
     return(
         <div className="body">
             <div className="search"></div>
-            <div className="res-container">
-                <div className="res-card">
-                    <img src={restro_img+"ue1tt2ujt2ybghd60bs0"} alt="" />
-                    <div className="restro-info">
-                        <div className="restro-title">Hotel Empire</div>
-                        <h4>"North Indian",
-    "Kebabs",
-    "Biryani"</h4>
-                        <h4>"4.1"</h4>
-                        <h4>34 MINS</h4>
-                        <h4>"₹450 FOR TWO"</h4>
-                    </div>
-                </div>
+            <div className="res-container">  
+                {card}
             </div>
         </div>
     )
--- src/components/Restro_Card.js
+++ src/components/Restro_Card.js
@@ -1,8 +1,19 @@
+import { restro_img } from "../utils/constants";
 export default function Restro_Cards(props){
-    console.log(props);
+    console.log(props.value);
+    const value=props.value
     return(
         <>
-
+            <div className="res-card">
+                    <img src={restro_img+value.cloudinaryImageId} alt="" />
+                    <div className="restro-info">
+                        <h3 className="restro-title">{value.name}</h3>
+                        <h4>{value.cuisines.join(", ")}</h4>
+                        <h4>{value.avgRating}</h4>
+                        <h4>{value.deliveryTime+" Mins"}</h4>
+                        <h4>{value.costForTwoString}</h4>
+                    </div>
+                </div>
         </>
     )
 }
