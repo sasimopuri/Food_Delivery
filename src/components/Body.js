b00c6b90d15ee726cd305a7004779c4903b8b967
sasimopuri <sasikmopuriu@gmail.com>
2023-04-04 17:20:19 +0530

    Added Top Rated restaurants filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,35 @@
-import Restro_Cards from "./Restro_Card"
-import { restro_img } from "../utils/constants"
+import RestroCards from "./RestroCard"
 import { restro_data } from "../utils/restro_data"
+import { useState } from "react"
 export default function Body(){
-    const card=restro_data?.map((data)=><Restro_Cards key={data.id} value={data.data} />)
-    // console.log("s",card);
+    const [restroData,setRestro_data]=useState(restro_data)
+    const [isTopRated,setIsTopRated]=useState(true)
+    const card=restroData?.map((data)=><RestroCards key={data.data.id} value={data} />)
+   
+    function handleTopRated(){
+        const topRated_data=restroData.filter((res)=>
+            res.data.avgRating>4.2
+        )
+        setRestro_data(topRated_data)
+        setIsTopRated((presTopRated)=>!presTopRated)
+        if(isTopRated){
+            setRestro_data(restro_data)
+        }
+        
+    }
+
+    function handleSearch(){
+
+    }
+
     return(
-        <div className="body">
-            <div className="search"></div>
+        <div className="body-container">
+            <button className="toprated-btn" onClick={handleTopRated}>
+                {isTopRated?"Show all Restaurants" : "Top Rated Restaurants"}
+            </button>
+            <div className="search">
+                <input type="text" onChange={handleSearch}/>
+            </div>
             <div className="res-container">  
                 {card}
             </div>
---
+++ src/components/RestroCard.js
@@ -0,0 +1,18 @@
+import { restro_img } from "../utils/constants";
+export default function RestroCards(props){
+    const {cloudinaryImageId,name,cuisines,avgRating,deliveryTime,costForTwoString}=props.value?.data
+    return(
+        <>
+            <div className="res-card">
+                    <img src={restro_img+cloudinaryImageId} alt="" />
+                    <div className="restro-info">
+                        <h3 className="restro-title">{name}</h3>
+                        <h4>{cuisines.join(", ")}</h4>
+                        <h4>{avgRating}</h4>
+                        <h4>{deliveryTime+" Mins"}</h4>
+                        <h4>{costForTwoString}</h4>
+                    </div>
+                </div>
+        </>
+    )
+}
