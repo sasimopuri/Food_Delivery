ed4d2067fe28022a288e748a10090f05207b7ad0
sasimopuri <115964652+sasimopuri@users.noreply.github.com>
2023-04-15 17:11:28 +0530

    Fetching data api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,28 @@
 import RestroCards from "./RestroCard"
-import { restro_data } from "../utils/restro_data"
-import { useState } from "react"
+// import { restro_data } from "../utils/restro_data"
+import { useEffect, useState } from "react"
+import { json } from "react-router-dom"
+import Shimmer from "./shimmer"
 export default function Body(){
-    const [restroData,setRestro_data]=useState(restro_data)
+    const [restroData,setRestro_data]=useState([])
     const [isTopRated,setIsTopRated]=useState(true)
     const card=restroData?.map((data)=><RestroCards key={data.data.id} value={data} />)
-   
+   console.log(restroData,"restro");
+
+    useEffect(()=>{
+        getRestaurants()
+    },[])
+
+    async function getRestaurants(){
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9304278&lng=77.678404&page_type=DESKTOP_WEB_LISTING"
+        );  
+        const json= await data.json();
+        // console.log(json);
+        const json_data=json?.data?.cards[2]?.data?.data?.cards
+        setRestro_data(json_data)
+    }   
+
     function handleTopRated(){
         const topRated_data=restroData.filter((res)=>
             res.data.avgRating>4.2
@@ -13,7 +30,7 @@ export default function Body(){
         setRestro_data(topRated_data)
         setIsTopRated((presTopRated)=>!presTopRated)
         if(isTopRated){
-            setRestro_data(restro_data)
+            setRestro_data(json)
         }
         
     }
@@ -29,7 +46,7 @@ export default function Body(){
 
     }
 
-    return(
+    return (restroData.length===0)? <Shimmer /> : (
         <div className="body-container">
             <div className="filters">
                 <div className="toprated-btn-container">
