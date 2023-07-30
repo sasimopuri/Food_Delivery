ad00f4668c5d1abad979f006865088f850421edd
sasimopuri <115964652+sasimopuri@users.noreply.github.com>
2023-07-30 16:04:30 +0530

    custom hook to fetch restroInfo

---
+++ src/utils/useRestaurants.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import { restro_menu } from "./constants";
+
+const useRestaurantMenu = (id) => {
+  const [resInfo, setResInfo] = useState();
+  useEffect(() => {
+    getRestaurantMenu();
+  });
+
+  const getRestaurantMenu = async () => {
+    const apiresult = await fetch(restro_menu + id);
+    const data = await apiresult.json();
+    setResInfo(data);
+  };
+
+  return resInfo;
+};
+
+export default useRestaurantMenu;
