297f074f1e4eb364ec41a73f19ed728b3213d6df
sasimopuri <115964652+sasimopuri@users.noreply.github.com>
2023-07-30 14:48:05 +0530

    installed tailwind css

--- src/components/Restaurants_menu.js
+++ src/components/Restaurants_menu.js
@@ -4,7 +4,6 @@ import { useParams } from "react-router-dom";
 
 const Restaurantsmenu=()=>{
     const {id}=useParams();
-
     const [restaurantMenu,setRestaurantsMenu]=useState(null)
     useEffect(()=>{
         getRestaurantsMenu()
