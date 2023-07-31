486a00a5471b5e36f1077186fe78ec523763cc84
sasimopuri <115964652+sasimopuri@users.noreply.github.com>
2023-07-31 23:56:09 +0530

    created hook to call restro menu api

--- src/components/Restaurants_menu.js
+++ src/components/Restaurants_menu.js
@@ -1,32 +1,45 @@
 import { useEffect, useState } from "react";
 import { restro_menu } from "../utils/constants";
 import { useParams } from "react-router-dom";
+import useRestaurantMenu from "../utils/useRestaurants";
+import Shimmer from "./shimmer";
+import RestaurantCategory from "./RestaurantCategory";
 
-const Restaurantsmenu=()=>{
-    const {id}=useParams();
-    const [restaurantMenu,setRestaurantsMenu]=useState(null)
-    useEffect(()=>{
-        getRestaurantsMenu()
-    },[])
+const Restaurantsmenu = () => {
+  const { id } = useParams();
+  const resInfo = useRestaurantMenu(id);
+  if (resInfo === null || resInfo === undefined) {
+    return <Shimmer />;
+  }
 
-    async function getRestaurantsMenu(){
-        const apiresult=await fetch(
-            restro_menu+id
-        )
-        const data=await apiresult.json();
-        console.log(data);
-        const result =  data?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards
-        setRestaurantsMenu(result)
-    }
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.data?.cards[0]?.card?.card?.info;
+  const categories =
+    resInfo?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
+      (res) =>
+        res?.card?.card?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+  console.log(categories);
+  return (
+    <>
+      <div className="w-6/12 flex items-center mx-auto flex-col">
+        <h1 className="text-2xl my-5 font-bold">{name}</h1>
+        <p className="text-base font-bold">
+          {cuisines.join(", ")} - {costForTwoMessage}
+        </p>
+        {categories.map((card) => (
+          <RestaurantCategory data={card?.card} />
+        ))}
+      </div>
+    </>
+  );
 
-    const menuItem=restaurantMenu?.map((item)=>(
-        <h1 key={item?.card?.info?.id}>{item?.card?.info?.name}</h1>))
+  // const menuItem = resInfo?.map((item) => (
+  //   <h1 key={item?.card?.info?.id}>{item?.card?.info?.name}</h1>
+  // ));
 
-    return(
-        <>
-            {menuItem}
-        </>
-    )
-}
+  // return <>{menuItem}</>;
+};
 
 export default Restaurantsmenu;
--- src/utils/useRestaurants.js
+++ src/utils/useRestaurants.js
@@ -5,7 +5,7 @@ const useRestaurantMenu = (id) => {
   const [resInfo, setResInfo] = useState();
   useEffect(() => {
     getRestaurantMenu();
-  });
+  },[]);
 
   const getRestaurantMenu = async () => {
     const apiresult = await fetch(restro_menu + id);
