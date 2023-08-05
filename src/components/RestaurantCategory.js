743771584ca5f68607840e72a70061df156dfd8c
sasimopuri <115964652+sasimopuri@users.noreply.github.com>
2023-08-05 12:06:50 +0530

    added accordion

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,18 +1,24 @@
+import { useState } from "react";
 import Restaurant_Items_List from "./Restaurant_Items_Litst";
 
 const RestaurantCategory = ({ data }) => {
-    console.log(data);
+  const [showMenuItems,setShowMenuItems]=useState(false)
+  
+  const handleAccordion=()=>{
+    setShowMenuItems(prev=>(!prev))
+  }
+
   return (
-    <div className="flex shadow-lg my-2 p-4 bg-gray-50 flex-col">
-      <div className="flex justify-between font-medium text-xl pb-2 border-gray-300 border-b-2">
+    <div className="flex shadow-lg my-2 p-4 w-full bg-gray-50 flex-col border-2">
+      <div className="flex justify-between font-medium text-xl pb-2 " onClick={handleAccordion}>
         <span>
           {data?.card?.title} ({data?.card?.itemCards.length})
         </span>
         <span>⬇️</span>
       </div>
       <div className="flex items-center flex-wrap">
-        {data?.card?.itemCards?.map((item)=>(
-            <Restaurant_Items_List item={item}/>
+        {data?.card?.itemCards?.map((item) => (
+          showMenuItems && <Restaurant_Items_List item={item} />
         ))}
       </div>
     </div>
