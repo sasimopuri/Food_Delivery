31780f8b65511b6153b757b58ff86feea956b0be
sasimopuri <115964652+sasimopuri@users.noreply.github.com>
2023-08-06 18:18:22 +0530

    fixed according minor issue

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -3,11 +3,11 @@ import Restaurant_Items_List from "./Restaurant_Items_Litst";
 
 const RestaurantCategory = ({ data, showMenuItems, setShowIndex }) => {
   return (
-    <div
-      className="flex shadow-lg my-2 p-4 w-full bg-gray-50 flex-col border-2"
-      onClick={() => setShowIndex()}
-    >
-      <div className="flex justify-between font-medium text-xl pb-2 ">
+    <div className="flex shadow-lg my-2 p-4 w-full bg-gray-50 flex-col border-2">
+      <div
+        className="flex justify-between font-medium text-xl pb-2 "
+        onClick={() => setShowIndex()}
+      >
         <span>
           {data?.card?.title} ({data?.card?.itemCards.length})
         </span>
@@ -15,7 +15,10 @@ const RestaurantCategory = ({ data, showMenuItems, setShowIndex }) => {
       </div>
       <div className="flex items-center flex-wrap">
         {data?.card?.itemCards?.map(
-          (item) => showMenuItems && <Restaurant_Items_List item={item} />
+          (item) =>
+            showMenuItems && (
+              <Restaurant_Items_List item={item} key={item?.card?.info?.name} />
+            )
         )}
       </div>
     </div>
